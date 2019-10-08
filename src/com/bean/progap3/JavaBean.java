38436b7005a1ef15d7801f2e97daceec729e7fb6
Jaime Bisuna <jaimebisona@gmail.com>
2019-10-08 15:37:23 +0800

    Added account page

--- src/com/bean/progap3/JavaBean.java
+++ src/com/bean/progap3/JavaBean.java
@@ -1,14 +1,25 @@
 package com.bean.progap3;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
+@SessionScoped
 public class JavaBean {
 	private String firstName;
 	private String lastName;
 	private int age;
 	private String contactNumber;
 	private String email;
+	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	private Map<String, String> auditTrail = new LinkedHashMap<String, String>();
 
 	public JavaBean() {
 		// TODO Auto-generated constructor stub
@@ -54,7 +65,30 @@ public class JavaBean {
 		this.email = email;
 	}
 
+	public Map<String, String> getAuditTrail() {
+		return auditTrail;
+	}
+
+	public void setAuditTrail(Map<String, String> auditTrail) {
+		this.auditTrail = auditTrail;
+	}
+
+	public String update() {
+		setFirstName(firstName);
+		setLastName(lastName);
+		setEmail(email);
+		setContactNumber(contactNumber);
+		return "homepage.xhtml";
+	}
+	
+	public String logout() {
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		return "index.xhtml?faces-redirect=true";
+	}
+
 	public String proceed() {
+		Date date = new Date();
+		auditTrail.put("Login", dateFormat.format(date).toString());
 		return "homepage";
 	}
 
